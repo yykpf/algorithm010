c41b29a0648bf66f3a8f4d26da55397e3a343f8b
yangyang1 <451368096@qq.com>
2020-06-17 09:56:01 +0800

    第二周

--- Week02/README.md
+++ Week02/README.md
@@ -1,8 +1,13 @@
 hash table
     哈希表也叫散列表
-    查询时间复杂度 o(1)[不包括有碰撞的情况，如果有大量碰撞会退化成o(n)]
+    查询时间复杂度 O(1)[不包括有碰撞的情况，如果有大量碰撞会退化成o(n)]
 面试做题法
     1、clarification
     2、possible solutions -> optimal (time & space)
     3、code
     4、test code
+二叉搜索树
+    插入、删除、查找 O(logn)
+
+    测试地址：
+        visualgo.net/zh/bst
--- Week02/test.php
+++ Week02/test.php
@@ -115,25 +115,102 @@
 // echo $n->longestCommonPrefix(["flower","flow","flight"]);
 
 // 字母异位词分组 https://leetcode-cn.com/problems/group-anagrams/
+// class Solution {
+//     /**
+//      * @param String[] $strs
+//      * @return String[][]
+//      */
+//     function groupAnagrams($strs) {
+//         // 算术基本定理可表述为：任何一个大于1的自然数 N,如果N不为质数，那么N可以唯一分解成有限个质数的乘积N=P1a1P2a2P3a3......Pnan，这里P1<P2<P3......<Pn均为质数，其中指数ai是正整数
+//         if (count($strs) <= 1)  return [$strs];
+//         $hash = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103];
+//         $ret = [];
+//         foreach ($strs as $value) {
+//             $strlen = 1;
+//             for ($i=0; $i < strlen($value); $i++) {
+//                 $strlen *= $hash[ord($value[$i])-97];
+//             }
+//             $ret[$strlen][] = $value;
+//         }
+//         return array_values($ret);
+//     }
+// }
+// $n = new Solution();
+// var_dump($n->groupAnagrams([""]));
+
+// N叉树的前序遍历
+// class Solution {
+//     function preorder($root) {
+//         $stack = new SplStack();
+//         $stack->push($root);
+//         $res = [];
+//         while (!$stack->isEmpty()) {
+//             $node = $stack->pop();
+//             $res[] = $node->val;
+//             for($i=count($node->children)-1;$i>=0;--$i) {
+//                 $stack->push($node->children[$i]);
+//             }
+//         }
+//         return $res;
+//     }
+// }
+
+// 验证二叉搜索树
+// class Solution {
+
+//     /**
+//      * @param TreeNode $root
+//      * @return Boolean
+//      */
+//     function isValidBST($root) {
+//         $this->help($root,-2 << 32,2 << 32);
+//     }
+//     function help($root,$min,$max) {
+//         if ($root == null) return true;
+//         if ($root->val <= $min || $root->val >= $max) return false;
+//         return $this->help($root->left,$min,$root->val) && $this->help($root->right,$root->val,$max);
+//     }
+// }
+
+// N叉树的最大深度
+// class Solution {
+//     /**
+//      * @param Node $root
+//      * @return integer
+//      */
+//     function maxDepth($root) {
+//         if ($root == null) {
+//             return 0;
+//         } elseif(empty($root->children)) {
+//             return 1;
+//         }else {
+//             $res = 0;
+//             for($i=0;$i<count($root->children);$i++) {
+//                 $temp = $this->maxDepth($root->children[$i]);
+//                 $res = max($temp,$res);
+//             }
+//             return $res;
+//         }
+//     }
+// }
+
+// N叉树的后序遍历
 class Solution {
     /**
-     * @param String[] $strs
-     * @return String[][]
+     * @param Node $root
+     * @return integer[]
      */
-    function groupAnagrams($strs) {
-        // 算术基本定理可表述为：任何一个大于1的自然数 N,如果N不为质数，那么N可以唯一分解成有限个质数的乘积N=P1a1P2a2P3a3......Pnan，这里P1<P2<P3......<Pn均为质数，其中指数ai是正整数
-        if (count($strs) <= 1)  return [$strs];
-        $hash = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103];
-        $ret = [];
-        foreach ($strs as $value) {
-            $strlen = 1;
-            for ($i=0; $i < strlen($value); $i++) {
-                $strlen *= $hash[ord($value[$i])-97];
-            }
-            $ret[$strlen][] = $value;
+    function postorder($root) {
+        $res = [];
+        $this->post($root,$res)
+        $res[] = $root->val;
+        return $res;
+    }
+    function post($root,&$res) {
+        if ($root == null) return $res;
+        for($i=count($root->children)-1;$i>=0;--$i) {
+            $res = $this->post($root->children[$i],$res);
+            $res[] = $root->children[$i]->val;
         }
-        return array_values($ret);
     }
 }
-$n = new Solution();
-var_dump($n->groupAnagrams([""]));
